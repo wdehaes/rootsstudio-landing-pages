f7325e4e0b5e573a069a50c55f1f629f1f951e7a
Willem Dehaes <willemdehaes@gmail.com>
2024-05-31 02:21:12 -0400

    switch to nuxt

---
+++ pages/natgeo.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="w-full bg-black">
+    <div class="max-w-[480px] mx-auto">
+      <header class="pt-16 flex flex-row justify-center">
+        <img src="~/assets/img/logos.svg" alt="Rurban X Nat Geo" />
+      </header>
+      <main>
+        <div class="flex flex-col gap-8 py-8">
+          <p
+            class="text-center text-white text-sm font-medium font-['League Spartan']"
+          >
+            Combining Craftsmanship, Impact, and Intention
+          </p>
+          <video
+            src="~/assets/video/weaving.mp4"
+            playsinline
+            loop
+            autoplay
+            muted
+            ref="target"
+          ></video>
+
+          <div class="px-8 sm:px-12">
+            <p
+              class="text-center text-white text-xs font-normal font-['League Spartan']"
+            >
+              Perched high in one of the world’s remaining biodiversity
+              hotspots, Chizami village lies in the hilly forests of Nagaland,
+              one of the “Seven Sister ‘’ states in Northeast India. Home to the
+              Chakhesang Naga tribe, the local lifestyle in Chizami is largely
+              agrarian, centered around terrace farming in the mountainous
+              terrains.
+            </p>
+          </div>
+          <div>
+            <img
+              src="~/assets/img/Nagaland_July_2016.jpg"
+              alt="Nagaland July 2016"
+            />
+          </div>
+          <div class="px-8 sm:px-12">
+            <p
+              class="text-center text-white text-xs font-normal font-['League Spartan']"
+            >
+            The Chakhesang Nagas are practitioners of weaving their heritage loin cloth. From growing their own cotton, hand-spinning the yarn to weaving, Chizami women have great intergenerational expertise in weaving heritage textiles on the backstrap loin loom — one of the oldest looms still used by humans.
+            </p>
+          </div>
+          <div class="flex flex-row gap-3">
+            <div class="flex flex-col gap-3">
+              <div><img src="~/assets/img/weaving-1.jpg" alt="weaving" /></div>
+              <div><img src="~/assets/img/weaving-2.jpg" alt="weaving" /></div>
+            </div>
+            <div>
+              <img src="~/assets/img/weaving-3.jpg" alt="weaving" />
+            </div>
+          </div>
+
+          <div class="px-8 sm:px-12">
+            <p
+              class="text-center text-white text-xs font-normal font-['League Spartan']"
+            >
+              This portable device is characterized by a strap fixed to one end
+              of the loom which the weavers wear around their waist, while
+              individual straps on the other end are anchored to a tree or pole.
+            </p>
+          </div>
+          <img
+            src="~/assets/img/Chizami logo.png"
+            alt=""
+            class="h-16 w-auto mx-auto"
+          />
+          <div class="flex flex-row gap-3 w-full">
+            <div><img src="~/assets/img/weaving-4.jpg" alt="weaving" /></div>
+            <div><img src="~/assets/img/weaving-5.jpg" alt="weaving" /></div>
+          </div>
+          <div class="px-8 sm:px-12">
+            <p
+              class="text-center text-white text-xs font-normal font-['League Spartan']"
+            >
+              This special product is created in collaboration between Rurban by
+              Roots Studio and National Geographic, to celebrate the masterful
+              Indigenous Chizami weaves from Nagaland, India. It has been
+              handwoven using 100% cotton yarn. It will keep you steady wherever
+              life takes you, interwoven with National Geographic’s flag colors
+              representing sea, land, and sky.
+            </p>
+          </div>
+          <div>
+            <img src="~/assets/img/map.svg" alt="Rurban X Nat Geo" />
+          </div>
+          <div class="px-8 sm:px-12">
+            <p
+              class="text-center text-white text-xs font-normal font-['League Spartan']"
+            >
+              Roots Studio reimagines cultural preservation and authorship by
+              bridging Indigenous artists from around the world and global
+              markets, with respect, reciprocity, and remuneration. For 10
+              years, they have built an IP protected library of traditional art
+              and knowledge, with royalties traceable back to every artist and
+              community. 
+            </p>
+          </div>
+          <div>
+            <img src="~/assets/img/weaving-6.jpg" alt="Rurban X Nat Geo" />
+          </div>
+        </div>
+      </main>
+      <footer class="flex flex-row justify-center pb-16">
+        <div class="flex-col justify-center items-center gap-[7px] inline-flex mx-auto">
+          <div
+            class="text-center text-white text-xs font-normal font-['League Spartan'] leading-[14.40px]"
+          >
+            Follow our journey<br /><br />
+          </div>
+          <div class="justify-center items-center gap-2.5 inline-flex">
+            <div class="text-center">
+              <span
+                class="text-white text-xs font-normal font-['League Spartan'] leading-[17.52px]"
+              >
+              </span
+              ><span
+                class="text-white text-[10px] font-normal font-['Content'] leading-[14.60px]"
+                >@</span
+              ><span
+                class="text-white text-xs font-normal font-['League Spartan'] leading-[17.52px]"
+                >rootsstudio<br />www.rootsstudio.com<br /><br
+              /></span>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
