1b83490c7e5ee57375e1b504db6f5117d9140baf
Willem Dehaes <willemdehaes@gmail.com>
2024-05-31 20:46:34 -0400

    add pwa support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,31 @@ export default defineNuxtConfig({
     url: "https://rootsstudio.site",
     name: "Rurban X NatGeo",
   },
+  head: {
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+    ],
+  },
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -13,6 +36,6 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    '/': { redirect: '/natgeo' },
-},
-})
+    "/": { redirect: "/natgeo" },
+  },
+});
